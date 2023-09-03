d6468a738e63a84b5066408c0cf5500f4dc05c72
JhyunJ1 <JhyunJ1005@gmail.com>
2023-09-03 23:10:06 +0900

    feat: register view

---
+++ htp_test/src/views/Register.vue
@@ -0,0 +1,64 @@
+<template>
+    <UpperSide msg="회원가입"/>
+    <div class="circle-box">
+      <div class="circle" style="background-color:gray; margin-left: 70px;"><p style="color: white; size: 5px;">약관 동의</p></div>
+      <hr/>
+      <div class="circle" id="c2"></div>
+      <hr/>
+      <div class="circle" id="c3"></div>
+    </div>
+</template>
+  
+  <script>
+  import UpperSide from '../components/UpperSide.vue';
+  
+  export default {
+    name: 'Register',
+    components: {
+      UpperSide
+    }
+  }
+  </script>
+  
+  <style>
+   body {
+      margin: 0px;
+      padding: 0px;
+    }
+    .circle-box {
+      display: table;
+      padding: 30px;
+      padding-left:290px;
+    }
+    hr {
+      margin-left: 20px;
+      margin-right: 20px;
+      margin-top: 40px;
+      border: none;
+      border-top: 3px dotted gainsboro;
+      color: white;
+      background-color: white;
+      height: 1px;
+      width: 300px;
+    }
+    .circle {
+      display: table-cell;
+     
+      vertical-align: center;
+      width:90px;
+      height:90px;
+      background-color: gainsboro;
+      border-radius: 50%;
+    }
+    .circle #c1 {
+      background-color: gray;
+    }
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+  </style>
+  
