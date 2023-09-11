a8752371e6823cee640095f89d4e8e3b9096dd47
JhyunJ1 <JhyunJ1005@gmail.com>
2023-09-11 23:48:21 +0900

    feat: create member modify page

--- htp_test/src/components/Mypage.vue
+++ htp_test/src/components/Mypage.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="main">
+    <div class="wrap">
+    <div class="main" style="float: left;">
         <img src="..\assets\HTP_logo2.png" class="logo">
         <!-- 글씨 크기 조절 (h1, h3 이런거 말고) -->
         <p class="title">안녕하세요, {{ user }}님!</p>
@@ -8,25 +9,59 @@
             <h2 class="more">&gt;</h2>
         </div>
         <hr>
-        <div class="menu">
+        <div class="menu" @click="modify_exec">
             <h2>회원 정보 수정</h2>
             <h2 class="more">&gt;</h2>
         </div>
         <hr>
         <p class="withdrawal">회원탈퇴</p>
     </div>
+
+    <div class="page" style="float: left;">
+        <Setting v-if = "this.modify"></Setting>
+    </div>
+    </div>
+
 </template>
 
 <script>
+import Setting from "./Setting.vue";
+
 export default {
     name: 'MypageComponent',
+    components: {
+        Setting
+    },
     props: {
-        user: String
+        user: String,
+        
+    },
+    data() {
+        return {
+            res: false,
+            modify: false,
+        }
+    },
+    methods: {
+        modify_exec: function () {
+            if (this.modify == false) {
+                this.modify = true;
+                this.res = false;
+            }
+            else {
+                this.modify = false;
+            }
+            return true
+        }
     }
 }
 </script>
 
 <style scoped>
+    .wrap {
+        display: flex;
+        flex-direction: row;
+    }
     .main {
         display: block;
         width: 32%;
@@ -92,5 +127,9 @@ export default {
         margin-top: 40vh;
         margin-left: 20%;
         padding-bottom: 30px;
+    }
+
+    .page {
+        flex-grow: 1;
     }
 </style>
