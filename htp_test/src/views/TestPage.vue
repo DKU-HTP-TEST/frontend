afc860f346dd205976205667d9f298e6a16125f0
JhyunJ1 <JhyunJ1005@gmail.com>
2023-11-10 18:03:08 +0900

    fix: foreign key integrity error

--- htp_test/src/views/TestPage.vue
+++ htp_test/src/views/TestPage.vue
@@ -71,54 +71,16 @@
             img: require("../assets/man-icon.png")
           }
         ],
-
-        imageUrl: null 
+        token: localStorage.getItem('token'),
+        imageUrl: null,
       }
     },
     methods: {
       next: function () {
-
         if (!this.imageUrl) {
           window.alert('이미지를 업로드해주세요');  //이미지 업로드가 안되면 모달 띄우기
         } else {
           this.imageUrl = null;  // 업로드한 이미지 초기화
-
-          /* 그림 보내서 결과 가져오기 */
-          switch(this.order) {
-            case 1:
-              let house_url = url + "analyze_img_house/"
-              var house_img
-              axios.post(house_url, new URLSearchParams(house_img))
-              .then((response) => {
-                
-                console.log(response)
-              })
-              .catch((error) => {
-                console.log(error)
-              })
-              break
-            case 2:
-              let tree_url = url + "analyze_img_tree/"
-              var tree_img
-              axios.post(tree_url, new URLSearchParams(tree_img))
-              .then((response) => {
-                console.log(response)
-              })
-              .catch((error) => {
-                console.log(error)
-              })
-            case 3:
-              let person_url = url + "analyze_img_person/"
-              var person_img
-              axios.post(person_url, new URLSearchParams(person_img))
-              .then((response) => {
-                console.log(response)
-              })
-              .catch((error) => {
-                console.log(error)
-              })
-          }
-
           if (this.order == 3) {
             this.$router.push({
               name: "Complete"
@@ -142,7 +104,8 @@
 
         axios.post(url, formData, {
           headers: {
-            'Content-Type': 'multipart/form-data'
+            'Content-Type': 'multipart/form-data',
+            Authorization: this.token,
           }
         })
         .then(response => {
