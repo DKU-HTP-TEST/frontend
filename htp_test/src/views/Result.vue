e6891c8db689650b27b67bfbad932f416633131b
dmatn0526 <dmstn_0526@naver.com>
2023-11-14 18:15:59 +0900

    fix: 공유 버튼 삭제

--- htp_test/src/views/Result.vue
+++ htp_test/src/views/Result.vue
@@ -16,7 +16,7 @@
       <div class="button">
         <button class="go_to_h" @click="go_main">처음으로</button>
         <button class="go_to_mp" @click="go_mp">마이페이지</button>
-        <button class="share">공유</button>
+        <!-- <button class="share">공유</button> -->
       </div>
     </div>
     
